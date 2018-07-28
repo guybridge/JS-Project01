a710e7a174ac1fce5838f488b90430847dc980ee
Guy Bridge <guy@bridgeit.com.au>
2018-07-29 07:34:47 +0800

    Added quote objects, added getRandomQuote() method and printQuote() method

---
+++ js/api/QuoteApi.js
@@ -0,0 +1,38 @@
+const quote1 = 
+{
+    quote: "Maybourne, you are an idiot every day of the week. Why couldn’t you have taken one day off?",
+    author: "Samantha Carter",
+    citation: "Stargate SG-1",
+    date: ""
+};
+
+const quote2 = 
+{
+    quote: "The Setesh guard’s nose drips.",
+    author: "Teal'c",
+    citation: "Stargate SG-1",
+    date: ""
+};
+const quote3 = 
+{
+    quote: "I do not fear computers. I fear the lack of them.",
+    author: "Isaac Asimov",
+    citation: "",
+    date: ""
+};
+
+const quote4 = 
+{
+    quote: "How inappropriate to call this planet 'Earth', when it is clearly 'Ocean'",
+    author: "Arthur C. Clarke",
+    citation: "",
+    date: ""
+};
+
+const quote5 = 
+{
+    quote: "Science knows still practically nothing about the real nature of matter, energy, dimension, or time… But whatever the meaning and purpose of this universe, you are a legitimate part of it.",
+    author: "Gene Roddenberry",
+    citation: "",
+    date: ""
+};
--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
 // FSJS - Random Quote Generator
+// Author: Guy Bridge
+// Date: 28/07/2018
 
 // Create the array of quote objects and name it quotes
 
 
 
-// Create the getRandomQuuote function and name it getRandomQuote
+let quotes = [quote1, quote2, quote3, quote4, quote5];
 
-
+// Create the getRandomQuote function and name it getRandomQuote
+function getRandomQuote()
+{
+    let randomNumber = Math.random();
+    return Math.floor(randomNumber * 6);
+}
 
 // Create the printQuote funtion and name it printQuote
-
+function printQuote()
+{
+    let quote = document.getElementsByClassName("quote");
+    quote.innerHTML = "Test";
+}
 
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+printQuote();
